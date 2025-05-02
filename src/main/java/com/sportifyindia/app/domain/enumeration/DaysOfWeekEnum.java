82cba7d60efc6e70c6f4aeb8ba654c8e81378591
vigneshsekar <vigneshsekar94@yahoo.com>
2025-05-02 22:44:29 +0530

    Subscription plan creation

---
+++ src/main/java/com/sportifyindia/app/domain/enumeration/DaysOfWeekEnum.java
@@ -0,0 +1,14 @@
+package com.sportifyindia.app.domain.enumeration;
+
+/**
+ * The DaysOfWeekEnum enumeration.
+ */
+public enum DaysOfWeekEnum {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY,
+}
